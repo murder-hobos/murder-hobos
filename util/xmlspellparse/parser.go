af6f81c4e8a49ae0966fc5849d9c731200f3c38f
Jaden Young <jaden.young44@gmail.com>
2016-11-12 23:05:31 -0600

    Added method to convert from xmlspell to dbspell

--- util/xmlspellparse/parser.go
+++ util/xmlspellparse/parser.go
@@ -1,8 +1,11 @@
 package xmlspellparse
 
 import (
+	"bytes"
 	"database/sql"
 	"encoding/xml"
+	"errors"
+	"html"
 	"log"
 	"strings"
 )
@@ -58,7 +61,6 @@ type DbSpell struct {
 	Duration      string `db:"duration"`
 	Range         string `db:"range"`
 	Components    *Components
-	MaterialDesc  string `db:"material_desc"`
 	Concentration bool   `db:"concentration"`
 	Ritual        bool   `db:"ritual"`
 	Description   string `db:"description"`
@@ -78,6 +80,81 @@ type Class struct {
 	ID        int    `db:"id"`
 	Name      string `db:"name"`
 	BaseClass int    `db:"base_class_id"`
+}
+
+func (d *DbSpell) FromXMLSpell(x *XMLSpell) error {
+	// vars we need to do a little work for
+	// to convert
+	var school, desc string
+	var comps *Components
+	var concentration, ritual bool
+
+	// We're not worrying about EE spells right now
+	if strings.Contains(x.Name, "(EE)") {
+		return errors.New("Elemental Evil spell")
+	}
+
+	// We want the long version, not the abbreviation
+	if s, ok := schools[x.School]; ok {
+		school = s
+	} else {
+		return errors.New("Not in schools map")
+	}
+
+	var b bytes.Buffer
+
+	// Texts is an index of strings, which conveniently map
+	// to paragraphs in the spell description text. The file
+	// uses <text/> elements as line breaks, but we ignore
+	// them here because we'll be rendering in html,
+	// so <p> tags arround paragraphs will be ideal. Also
+	// note that we are storing escaped html into our db.
+	for _, text := range x.Texts {
+		if text != "" {
+			b.Write([]byte(surround(text, "<p>", "</p>")))
+		}
+		// This is dirty, but the file doesn't have a field
+		// for concentation, only way to find it is to see
+		// if the description mentions it.
+		if strings.Contains(text, "concentration") {
+			concentration = true
+		}
+	}
+	desc = b.String()
+
+	comps = parseComponents(x.Components)
+
+	// In the file, ritual will be either "" or "YES"
+	ritual = strings.Compare(x.Ritual, "YES") == 0
+
+	d.Name = x.Name
+	d.Level = x.Level
+	d.School = school
+	d.CastTime = x.Time
+	d.Duration = x.Duration
+	d.Range = x.Range
+	d.Components = comps
+	d.Concentration = concentration
+	d.Ritual = ritual
+	d.Description = desc
+	// EEEEWEWEWEWEWEWEEEEEEEEEEEWWWWWWWWWWWW
+	// Hard coding in PHB as user with ID 1 right now
+	// I may hate myself for it later.
+	d.SourceID = 1
+
+	return nil
+}
+
+// Surround places `start` and the beginning and `end` at the end of
+// an `original` string. Html characters are escaped.
+func surround(original, start, end string) string {
+	var b bytes.Buffer
+
+	b.Write([]byte(start))
+	b.Write([]byte(original))
+	b.Write([]byte(end))
+
+	return html.EscapeString(b.String())
 }
 
 // Capitalize a single char from a string at specified index
@@ -113,6 +190,7 @@ func parseComponents(s string) *Components {
 	mat = strings.Contains(s, "M")
 
 	// ('s only ocurr in our domain as the beginning of the material description
+	// Index returns -1 if not present
 	i := strings.Index(s, "(")
 	if i > -1 {
 		// Trim off parens
--- util/xmlspellparse/parser_test.go
+++ util/xmlspellparse/parser_test.go
@@ -2,8 +2,11 @@ package xmlspellparse
 
 import (
 	"database/sql"
+	"encoding/xml"
 	"reflect"
 	"testing"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 func Test_parseComponents(t *testing.T) {
@@ -116,6 +119,130 @@ func Test_toNullString(t *testing.T) {
 	for _, tt := range tests {
 		if got := toNullString(tt.args.s); !reflect.DeepEqual(got, tt.want) {
 			t.Errorf("%q. toNullString() = %v, want %v", tt.name, got, tt.want)
+		}
+	}
+}
+
+func TestDbSpell_FromXMLSpell(t *testing.T) {
+	type fields struct {
+		Name          string
+		Level         string
+		School        string
+		CastTime      string
+		Duration      string
+		Range         string
+		Components    *Components
+		Concentration bool
+		Ritual        bool
+		Description   string
+		SourceID      int
+	}
+	type args struct {
+		x *XMLSpell
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			"Zone of Truth. This should work.",
+			fields{
+				Name:     "Zone of Truth",
+				Level:    "2",
+				School:   "Enchantment",
+				CastTime: "1 action",
+				Duration: "10 minutes",
+				Range:    "60 feet",
+				Components: &Components{
+					Verb: true,
+					Som:  true,
+					Mat:  true,
+					Matdesc: sql.NullString{
+						String: "",
+						Valid:  false,
+					},
+				},
+				Concentration: false,
+				Ritual:        false,
+				Description:   "&lt;p&gt;You create a magical zone that guards against deception in a 15-foot-radius sphere centered on a point of your choice within range. Until the spell ends, a creature that enters the spell&#39;s area for the first time on a turn or starts its turn there must make a Charisma saving throw. On a failed save, a creature can&#39;t speak a deliberate lie while in the radius. You know whether each creature succeeds or fails on its saving throw.&lt;/p&gt;&lt;p&gt;An affected creature is aware of the spell and can thus avoid answering questions to which it would normally respond with a lie. Such creatures can be evasive in its answers as long as it remains within the boundaries of the truth.&lt;/p&gt;",
+				SourceID:      1,
+			},
+			args{
+				&XMLSpell{
+					XMLName:    xml.Name{"", "spell"},
+					Name:       "Zone of Truth",
+					Level:      "2",
+					School:     "EN",
+					Ritual:     "",
+					Time:       "1 action",
+					Range:      "60 feet",
+					Components: "V, S, M",
+					Duration:   "10 minutes",
+					Classes:    "Bard, Cleric, Paladin, Paladin (Devotion), Paladin (Crown)",
+					Texts: []string{"You create a magical zone that guards against deception in a 15-foot-radius sphere centered on a point of your choice within range. Until the spell ends, a creature that enters the spell's area for the first time on a turn or starts its turn there must make a Charisma saving throw. On a failed save, a creature can't speak a deliberate lie while in the radius. You know whether each creature succeeds or fails on its saving throw.",
+						"",
+						"An affected creature is aware of the spell and can thus avoid answering questions to which it would normally respond with a lie. Such creatures can be evasive in its answers as long as it remains within the boundaries of the truth.",
+					},
+				},
+			},
+			false,
+		},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		d := &DbSpell{
+			Name:          tt.fields.Name,
+			Level:         tt.fields.Level,
+			School:        tt.fields.School,
+			CastTime:      tt.fields.CastTime,
+			Duration:      tt.fields.Duration,
+			Range:         tt.fields.Range,
+			Components:    tt.fields.Components,
+			Concentration: tt.fields.Concentration,
+			Ritual:        tt.fields.Ritual,
+			Description:   tt.fields.Description,
+			SourceID:      tt.fields.SourceID,
+		}
+		var got DbSpell
+		got.FromXMLSpell(tt.args.x)
+		if !reflect.DeepEqual(&got, d) {
+			spew.Dump(&got)
+			spew.Dump(d)
+			t.Errorf("%q. DbSpell.fromXMLSpell() = %v, want %v", tt.name, got, d)
+		}
+		if err := d.FromXMLSpell(tt.args.x); (err != nil) != tt.wantErr {
+			t.Errorf("%q. DbSpell.fromXMLSpell() error = %v, wantErr %v", tt.name, err, tt.wantErr)
+		}
+	}
+}
+
+func Test_surround(t *testing.T) {
+	type args struct {
+		original string
+		start    string
+		end      string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			"Paragraph tags",
+			args{"spell description", "<p>", "</p>"},
+			"&lt;p&gt;spell description&lt;/p&gt;",
+		},
+		{
+			"No start/end",
+			args{"textextext", "", ""},
+			"textextext",
+		},
+	}
+	for _, tt := range tests {
+		if got := surround(tt.args.original, tt.args.start, tt.args.end); got != tt.want {
+			t.Errorf("%q. Surround() = %v, want %v", tt.name, got, tt.want)
 		}
 	}
 }
