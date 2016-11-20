31fffc4e2c2a7419d4dfd800d3da3d382df51283
Jaden Young <jaden.young44@gmail.com>
2016-11-19 20:51:00 -0600

    Added spell details page and basic spell LIST

--- model/models.go
+++ model/models.go
@@ -1,9 +1,26 @@
 package model
 
-import "database/sql"
+import (
+	"bytes"
+	"context"
+	"database/sql"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/jaden-young/murder-hobos/util"
+	"github.com/jmoiron/sqlx"
+)
+
+// DB is a wrapper for our db connection that we can use to define
+// queries on as methods
+type DB struct {
+	DB *sqlx.DB
+}
 
 // Spell represents our database version of a spell
 type Spell struct {
+	ID            int            `db:"id"`
 	Name          string         `db:"name"`
 	Level         string         `db:"level"`
 	School        string         `db:"school"`
@@ -31,4 +48,127 @@ type Class struct {
 type ClassSpells struct {
 	ClassID int `db:"class_id"`
 	SpellID int `db:"spell_id"`
+}
+
+// User represents a user in our application
+type User struct {
+	ID       int    `db:"id"`
+	Username string `db:"username"`
+	Password []byte `db:"password"`
+}
+
+// ComponentsStr returns a string representation of the
+// components for a spell.
+// Example:
+// 		"V, S, M (Some cool component no one will ever need because they have a focus)"
+func (s *Spell) ComponentsStr() string {
+	b := bytes.Buffer{}
+	if s.Verbal {
+		b.WriteString("V")
+	}
+	if s.Somatic {
+		b.WriteString("S")
+	}
+	if s.Material {
+		b.WriteString("M")
+	}
+
+	str := util.Intersperse(b.String(), ", ")
+	b.Reset()
+
+	// if not null
+	if s.MaterialDesc.Valid {
+		b.WriteString(" (")
+		b.WriteString(s.MaterialDesc.String)
+		b.WriteString(")")
+	}
+	return str + b.String()
+}
+
+// HTMLDescription returns the spell's Description as a template.HTML
+// so that the HTML in the description will be rendered instead of
+// escaped in go's templating engine
+func (s *Spell) HTMLDescription() template.HTML {
+	return template.HTML(s.Description)
+}
+
+// WithDB wraps a http.HandlerFunc with access to this database
+// connection by placing a reference with key "db" in the request's
+// context
+func (db *DB) WithDB(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.WithValue(r.Context(), "db", db)
+		r = r.WithContext(ctx)
+		fn(w, r)
+	}
+}
+
+// GetSpellByID searches `db` for a Spell row
+// with a matching id. If unsuccessful, an empty
+// spell is returned along with false for ok.
+func (db *DB) GetSpellByID(id int) (*Spell, bool) {
+	s := &Spell{}
+	if err := db.DB.Get(s, "SELECT * FROM Spell WHERE id=?", id); err != nil {
+		return &Spell{}, false
+	}
+	return s, true
+}
+
+// GetSpellByName searches `db` for a Spell row
+// with a matching `name` and a source_id in
+// `sourceIDs`. If unsuccessful, an empty
+// spell is returned along with false for ok.
+func (db *DB) GetSpellByName(name string, sourceIDs []string) (*Spell, bool) {
+	query, args, err := sqlx.In(`SELECT * FROM Spell
+								WHERE name=? AND
+								source_id in (?);`,
+		name, sourceIDs)
+	if err != nil {
+		log.Printf("Error preparing sqlx.In statement: %s\n", err.Error())
+		return &Spell{}, false
+	}
+	query = db.DB.Rebind(query)
+
+	s := &Spell{}
+	if err := db.DB.Get(s, query, args...); err != nil {
+		log.Printf("Error executing query %s\n %s\n", query, err.Error())
+		return &Spell{}, false
+	}
+	return s, true
+}
+
+// GetSpellClasses searches `db` and returns a slice of
+// Class objects available to the spell with `spellID`
+func (db *DB) GetSpellClasses(spellID int) (*[]Class, error) {
+	cs := &[]Class{}
+	err := db.DB.Select(cs, `SELECT C.id, C.name, C.base_class_id
+	 					  FROM Class AS C
+						  JOIN ClassSpells as CS ON
+						  C.id = CS.class_id
+						  JOIN Spell AS S ON
+						  CS.spell_id = S.id
+						  WHERE S.id = ?`,
+		spellID)
+	if err != nil {
+		return &[]Class{}, err
+	}
+	return cs, nil
+}
+
+// GetAllSpells returns a slice of every spell object in the database
+// with a source_id in `sourceIDs`
+func (db *DB) GetAllSpells(sourceIDs []string) (*[]Spell, bool) {
+	query, args, err := sqlx.In(`SELECT * FROM Spell WHERE source_id IN (?);`, sourceIDs)
+	if err != nil {
+		log.Printf("Error preparing sqlx.In statement: %s\n", err.Error())
+		return &[]Spell{}, false
+	}
+	query = db.DB.Rebind(query)
+
+	spells := &[]Spell{}
+	if err := db.DB.Select(spells, query, args...); err != nil {
+		log.Printf("Error executing query %s\n %s\n", query, err.Error())
+		return &[]Spell{}, false
+	}
+	return spells, true
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,25 +1,34 @@
 package routes
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 	"strings"
 
-	gmux "github.com/gorilla/mux"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+	mux "github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"github.com/jaden-young/murder-hobos/model"
+	"github.com/jmoiron/sqlx"
 )
 
 var (
 	// Package global that holds a map of our templates.
-	//
 	tmpls map[string]*template.Template
+
+	// DB connection
+	db *model.DB
+
+	// Session store
+	store = sessions.NewCookieStore([]byte("super-secret-key-that-is-totally-secure"))
 )
 
-// package initialization, sets up our tmpls map
 func init() {
-	log.Println("Initializing template map")
+	// setup template map
 	if tmpls == nil {
 		tmpls = make(map[string]*template.Template)
 	}
@@ -39,47 +48,132 @@ func init() {
 	for _, layout := range layouts {
 		files := append(includes, layout)
 		tmpls[filepath.Base(layout)] = template.Must(template.ParseFiles(files...))
-		log.Printf("Added to tmpl map: %s\n", filepath.Base(layout))
-	}
-}
-
-func renderTemplate(w http.ResponseWriter, name string, p *Page) error {
-	tmpl, ok := tmpls[name+".html"]
-	if !ok {
-		return fmt.Errorf("The template %s does not exist.", name)
 	}
 
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	return tmpl.ExecuteTemplate(w, "base", p)
-}
+	dbconfig := mysql.Config{
+		User:            os.Getenv("MYSQL_USER"),
+		Passwd:          os.Getenv("MYSQL_PASS"),
+		DBName:          os.Getenv("MYSQL_DB_NAME"),
+		Net:             "tcp",
+		Addr:            os.Getenv("MYSQL_ADDR"),
+		MultiStatements: false,
+	}
 
-// Page represents a basic page
-type Page struct {
-	Title string
-	Body  string
+	conn := sqlx.MustConnect("mysql", dbconfig.FormatDSN())
+	db = &model.DB{DB: conn}
+
 }
 
 // New returns an initialized gorilla *mux.Router with all of our routes
-func New() *gmux.Router {
-	r := gmux.NewRouter()
+func New() *mux.Router {
+	r := mux.NewRouter()
 
 	r.HandleFunc("/", indexHandler)
+	r.HandleFunc("/spell/{spellName}", db.WithDB(withSourceIDs(spellDetailsHandler)))
+	r.HandleFunc("/spells", db.WithDB(withSourceIDs(spellsHandler)))
 	r.PathPrefix("/static").HandlerFunc(staticHandler)
 	return r
 }
 
+// Index doesn't really do much for now
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	p := &Page{Title: "Murder Hobos", Body: "Welcome to Murder Hobos!"}
-	err := renderTemplate(w, "index", p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	if tmpl, ok := tmpls["index.html"]; ok {
+		tmpl.ExecuteTemplate(w, "base", nil)
+	} else {
+		errorHandler(w, r, http.StatusInternalServerError)
 	}
 }
 
+// /spells
+// List all spells. We really should chache this eventually
+// instead of hitting the db everytime
+func spellsHandler(w http.ResponseWriter, r *http.Request) {
+	sourceIDs := r.Context().Value("sourceIDs").([]string)
+	db := r.Context().Value("db").(*model.DB)
+
+	spells, ok := db.GetAllSpells(sourceIDs)
+	if !ok {
+		log.Printf("Error getting all spells\n")
+		errorHandler(w, r, http.StatusInternalServerError)
+		return
+	}
+
+	if tmpl, ok := tmpls["spells.html"]; ok {
+		tmpl.ExecuteTemplate(w, "base", spells)
+	} else {
+		errorHandler(w, r, http.StatusInternalServerError)
+	}
+}
+
+// Show information about a single spell
+func spellDetailsHandler(w http.ResponseWriter, r *http.Request) {
+	name := mux.Vars(r)["spellName"]
+	sourceIDs := r.Context().Value("sourceIDs").([]string)
+	db := r.Context().Value("db").(*model.DB)
+	var spell *model.Spell
+	var classes *[]model.Class
+
+	if s, ok := db.GetSpellByName(name, sourceIDs); ok {
+		spell = s
+	} else {
+		errorHandler(w, r, http.StatusNotFound)
+		log.Printf("Couldn't find spell with name: %s and ids: %s\n", name, sourceIDs)
+		return
+	}
+
+	if cs, err := db.GetSpellClasses(spell.ID); err == nil {
+		classes = cs
+	} else {
+		errorHandler(w, r, http.StatusInternalServerError)
+		log.Printf("Error getting spell classes: %s\n", err.Error())
+		return
+	}
+
+	if tmpl, ok := tmpls["spell-details.html"]; ok {
+		data := struct {
+			Spell   *model.Spell
+			Classes *[]model.Class
+		}{
+			spell,
+			classes,
+		}
+		tmpl.ExecuteTemplate(w, "base", data)
+	} else {
+		errorHandler(w, r, http.StatusInternalServerError)
+		log.Printf("Error loading template for spell-details\n")
+		return
+	}
+}
 func staticHandler(w http.ResponseWriter, r *http.Request) {
-	// Disable listing directories
+	// Don't want to list directories
 	if strings.HasSuffix(r.URL.Path, "/") {
 		http.Error(w, "File not found", http.StatusBadRequest)
 	}
 	http.ServeFile(w, r, r.URL.Path[1:])
+}
+
+// Custom stuff for errors
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+	w.WriteHeader(status)
+	var title, message string
+	var tmpl *template.Template
+
+	if t, ok := tmpls["error.html"]; ok {
+		tmpl = t
+	} else {
+		http.Error(w, "Server's busted.", http.StatusInternalServerError)
+	}
+
+	if status == http.StatusNotFound {
+		title = "Not Found"
+		message = "Whoops! We can't find that!"
+	}
+
+	if status == http.StatusInternalServerError {
+		title = "Server Error"
+		message = "Our server is having issues. >:("
+	}
+
+	vars := map[string]string{"Title": title, "Message": message}
+	tmpl.ExecuteTemplate(w, "base", vars)
 }
