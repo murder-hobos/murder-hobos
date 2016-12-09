4b2201353c3add35d77ed0fb9dc5a401fd53f68f
Jaden Young <jaden.young44@gmail.com>
2016-12-09 17:01:50 -0600

    Added user datastore methods

--- model/user.go
+++ model/user.go
@@ -1,5 +1,12 @@
 package model
 
+// UserDatastore describes methods available to our database
+// pertaining to users
+type UserDatastore interface {
+	GetUserByUsername(name string) (*User, bool)
+	GetUserByID(id int) (*User, bool)
+}
+
 // User represents a user in our application
 type User struct {
 	ID       int    `db:"id"`
@@ -15,6 +22,19 @@ func (db *DB) GetUserByUsername(name string) (*User, bool) {
 					  FROM User
 					  WHERE username = ?`,
 		name)
+	if err != nil {
+		return nil, false
+	}
+	return u, true
+}
+
+// GetUserByID returns a user object with matchin
+// id if found
+func (db *DB) GetUserByID(id int) (*User, bool) {
+	u := &User{}
+	err := db.Get(u, `SELECT id, username, password
+					  FROM User
+					  WHERE id=?`, id)
 	if err != nil {
 		return nil, false
 	}
