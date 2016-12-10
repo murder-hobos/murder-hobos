0afe8d2ee12ba598e938fd169a7756ba0b15873a
Jaden Young <jaden.young44@gmail.com>
2016-12-10 00:37:36 -0600

    YOLO

--- cmd/murder-hobos-server/server.go
+++ cmd/murder-hobos-server/server.go
@@ -5,22 +5,21 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/go-sql-driver/mysql"
 	"github.com/murder-hobos/murder-hobos/routes"
 )
 
 func main() {
 	// setup DB
-	dbconfig := mysql.Config{
-		User:            os.Getenv("MYSQL_USER"),
-		Passwd:          os.Getenv("MYSQL_PASS"),
-		DBName:          os.Getenv("MYSQL_DB_NAME"),
-		Net:             "tcp",
-		Addr:            os.Getenv("MYSQL_ADDR"),
-		MultiStatements: false,
-	}
+	//dbconfig := mysql.Config{
+	//	User:            os.Getenv("MYSQL_USER"),
+	//	Passwd:          os.Getenv("MYSQL_PASS"),
+	//	DBName:          os.Getenv("MYSQL_DB_NAME"),
+	//	Net:             "tcp",
+	//	Addr:            os.Getenv("MYSQL_ADDR"),
+	//	MultiStatements: false,
+	//}
 
-	r := routes.New(dbconfig.FormatDSN())
+	r := routes.New(os.Getenv("DATABASE_URL"))
 	port := os.Getenv("PORT")
 	log.Fatal(http.ListenAndServe(":"+port, r))
 }
